40151cf01dd1ff44efd03ba9f7e20e1c35fb8b9c
yahya <yahya1haloui@gmail.com>
2024-10-24 16:17:55 -0400

    class utilisateur

---
+++ AppPaiements/AppPaiements/Utilisateur.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    internal class Utilisateur 
+    {
+        public string nom { set; get; }
+        public List<Paiement> paiements { set; get; }
+
+        public Utilisateur(string nom, List<Paiement> paiements)
+        {
+            this.nom = nom;
+            this.paiements = new List<Paiement>() ;
+        }
+        public void afficherInfos()
+        {
+            Console.WriteLine($"nom : {nom}");
+            Console.WriteLine("list de paiement : ");
+            foreach (Paiement p in paiements)
+            {
+                p.AfficherDetaills();
+            }
+        }
+        public void ajouterPaiement (Paiement paiement)
+        {
+            paiements.Add(paiement);
+        }
+    }
+}
