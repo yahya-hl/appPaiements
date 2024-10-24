68ea1a400230daa80ee4e31763e7bf2218f31af3
yahya <yahya1haloui@gmail.com>
2024-10-24 14:35:02 -0400

    class carte credit

---
+++ AppPaiements/AppPaiements/Class2.cs
@@ -0,0 +1,27 @@
+﻿namespace AppPaiements
+{
+    public class CarteCredit : Paiement
+    {
+
+        public long numeroCarte {  get; set; }
+
+
+
+        public CarteCredit(long numeroCarte, double Montant, string Description)
+            : base(Montant,Description)
+        {
+            this.numeroCarte = numeroCarte;
+
+        } 
+        public new void AfficherDetaills()
+        {
+            Console.WriteLine($"Montant : {Montant}");
+            Console.WriteLine($" Description : {Description}");
+            Console.WriteLine($"numero de la carte : {numeroCarte}");
+        }
+
+
+    }
+    
+
+}
--- AppPaiements/AppPaiements/Program.cs
+++ AppPaiements/AppPaiements/Program.cs
@@ -1,7 +1,24 @@
-﻿internal class Program
+﻿namespace AppPaiements { 
+internal class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-    }
+            //etape 4
+
+            CarteCredit Carte1 = new(4666672238674564, 150.75, "Matériel de randonnée");
+            CarteCredit Carte2 = new(5675545364335544, 64.00, "Abonnement internet ");
+            CarteCredit Carte3 = new(4904835748454657, 50.45, "phone");
+
+            Carte1.AfficherDetaills();
+            Carte2.AfficherDetaills();
+            Carte3.AfficherDetaills();
+
+
+
+
+
+
+        }
+}
 }
