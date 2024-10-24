c1b5c37d4a1749638544e9d37bafd3f2e3132fe4
yahya <yahya1haloui@gmail.com>
2024-10-24 13:28:56 -0400

    class Paiement

---
+++ AppPaiements/AppPaiements/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    public class Paiement
+    {
+        public double Montant { get; set; }
+        public string Description { get; set; }
+
+        public Paiement(double Montant, string Description)
+        {
+            this.Montant = Montant;
+            this.Description = Description;
+
+        }
+
+        public void AfficherDetaills()
+        {
+            Console.WriteLine($"Montant : {Montant}, Description : {Description}");
+
+
+
+
+
+        }
+    }
+
+    
+}
